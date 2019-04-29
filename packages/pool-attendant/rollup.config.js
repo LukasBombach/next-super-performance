6298c4b3b81f227e0e9eccb6ec184b1f5ec985d4
Lukas Bombach <lukas.bombach@googlemail.com>
2019-04-29 15:35:30 +0200

    fixed rollup warnings

--- packages/pool-attendant/rollup.config.js
+++ packages/pool-attendant/rollup.config.js
@@ -3,11 +3,16 @@ import pkg from "./package.json";
 
 export default {
   input: "./lib/index.tsx",
-  output: "./dist/index.js",
   output: [
-    { file: pkg.browser, format: "umd", name: "poolAttendant" },
+    {
+      file: pkg.browser,
+      format: "umd",
+      name: "poolAttendant",
+      globals: { react: "React" }
+    },
     { file: pkg.main, format: "cjs" },
     { file: pkg.module, format: "es" }
   ],
+  external: ["react"],
   plugins: [typescript()]
 };
